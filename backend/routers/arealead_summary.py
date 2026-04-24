c23a29722fdcff4e6a766fc43d42fdb8e481feca
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-04-24 11:05:58 +0530

    Multi-select : Routers update enabled multi-select feature

--- backend/routers/arealead_summary.py
+++ backend/routers/arealead_summary.py
@@ -10,10 +10,10 @@ def get_filters():
 @router.get("/data")
 def get_data(
     request: Request,
-    region: str | None = Query(None),
-    area: str | None = Query(None),
-    year: str | None = Query(None),
-    month: str | None = Query(None),
+    region: list[str] | None = Query(None),
+    area: list[str] | None = Query(None),
+    year: list[str] | None = Query(None),
+    month: list[str] | None = Query(None),
     limit: int = Query(15),
     offset: int = Query(0)
 ):
@@ -28,10 +28,10 @@ def get_data(
 
 @router.get("/export")
 def export_data(
-    region: str | None = Query(None),
-    area: str | None = Query(None),
-    year: str | None = Query(None),
-    month: str | None = Query(None)
+    region: list[str] | None = Query(None),
+    area: list[str] | None = Query(None),
+    year: list[str] | None = Query(None),
+    month: list[str] | None = Query(None)
 ):
     from backend.services.export_utils import json_to_excel_streaming_response
     data = arealead_summary_service.get_arealead_summary_data(region, area, year, month, limit=100000, offset=0)
